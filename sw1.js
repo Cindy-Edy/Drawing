1b80ea0c880f9abff1ed7369c5a1347b99bf71cf
Cindy-Edy <ngrok496@gmail.com>
2025-05-10 14:40:18 +0700

    Add files via upload

---
+++ sw1.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open("keyzia-cache").then(function(cache) {
+      return cache.addAll([
+        "/",
+        "/index1.html",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png",
+        // Tambahkan file lain seperti CSS, JS, gambar jika ada
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
